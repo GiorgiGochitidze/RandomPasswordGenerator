f626d892a254c0d6b9e3d7389aad198161de342a
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-05-14 18:28:43 +0400

    clls 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+
+  const [numbers, setNumbers] = useState("1234567890")
+  const [symbols, setSymbols] = useState("!@#$%^&*")
+  const [upperCase, setUpperCase] = useState("QWERTYUIOPASDFGHJKLZXCVBNM")
+  const [lowerCase, setLowerCase] = useState("qwertyuiopasdfghjklzxcvbnm")
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+    let characters = '';
+    if (document.getElementById('numbers').checked) {
+      characters += numbers;
+    }
+    if (document.getElementById('symbols').checked) {
+      characters += symbols;
+    }
+    if (document.getElementById('upperCase').checked) {
+      characters += upperCase;
+    }
+    if (document.getElementById('lowerCase').checked) {
+      characters += lowerCase;
+    }
+    let password = '';
+    for (let i = 0; i < 12; i++) {
+      password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    setPassword(password);
+  }
+
+  return (
+    <div className='container'>
+      <h1 style={{ margin: '0 auto', color: 'white', paddingTop: '20px' }}>Random Password Generator</h1>
+      <div className='rnd-number-generator-container'>
+        <div className='result'>
+          <label>Result:</label>
+          <input disabled className='result-container' placeholder='P5g$%dsf@' value={password}></input>
+        </div>
+        <div className='checkbox1'>
+          <input id='upperCase' type='checkbox'></input>
+          <h3>Enable UpperCase</h3>
+        </div>
+        <div className='checkbox1'>
+          <input id='lowerCase' type='checkbox'></input>
+          <h3>Enable LowerCase</h3>
+        </div>
+        <div className='checkbox2'>
+          <input style={{ marginLeft: '-20px' }} id='symbols' type='checkbox'></input>
+          <h3>Enable Symbols</h3>
+        </div>
+        <div className='checkbox2'>
+          <input style={{ marginLeft: '-12px' }} id='numbers' type='checkbox'></input>
+          <h3>Enable Numbers</h3>
+        </div>
+        <button className='generate-button' onClick={generatePassword}>Generate Random Password</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
